cf06a98191b39646197b44a6e3df06d89bd913e1
pradeepkumar41296 <54176205+pradeepkumar41296@users.noreply.github.com>
2019-10-14 15:59:02 +1100

    Add files via upload

---
+++ EmailSender.cs
@@ -0,0 +1,30 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("noreply@localhost.com", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            Attachment newAttach = new Attachment(Server.MapPath(“~/MyFile.txt”));
+            newMsg.Attachments.Add(newAttach);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
